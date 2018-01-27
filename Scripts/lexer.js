976838ec9d9022296401959004eed7d645d2fccc
MAZimmermann <MAZimmermann26@gmail.com>
2018-01-27 16:16:17 -0500

    beginning project 1, added scripts directory

---
+++ Scripts/lexer.js
@@ -0,0 +1,9 @@
+function lex()
+
+{
+    // Grab source code from taInput
+    var sourceCode = document.getElementById("taInput").value;
+    // Trim the leading and trailing spaces.
+    sourceCode = trim(sourceCode);
+    return sourceCode;
+}
