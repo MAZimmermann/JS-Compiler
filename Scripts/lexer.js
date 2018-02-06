30c43b31e9c88d99ee1ba6d786a6a3c6e9a1aaf9
MAZimmermann <MAZimmermann26@gmail.com>
2018-02-05 20:36:28 -0500

    New textarea for testing, working on error/warning system

--- Scripts/lexer.js
+++ Scripts/lexer.js
@@ -24,17 +24,23 @@ function lex() {
 
     // Define/declare array of tokens to be returned
     var tokens = [];
+
     // Declare/define line number
     var lineNum = 0;
 
-    // TODO: Give credit for this
-    DELIMITER_PATTERN = /([a-z]+)|(\d+)|("[^"]*")|(\/\*[^\/\*]*\*\/)|(==)|(!=)|(\S)|(\n)/g;
+    // errors
+    var errors = [];
+    // warnings
+    var warnings = [];
+    // If an invalid lexeme is found,
+    //  we quit the lexer and return the location of the invalid lexeme
+    var invalidLexeme = false;
 
+    // TODO: Give credit for this;
     // TODO: Determine/write other portions ^delimiter pattern^ for clarification
-    var _numberPattern = /\d+/;
-    var _charPattern = /[a-z]/;
-    var _stringPattern = /"[^"]*"/;
-    var _idPattern = /[a-z]+/;
+    DELIMITER_1 = /([a-z]+)|(\d+)|("[^"]*")|(\/\*[^\/\*]*\*\/)|(==)|(!=)|(\S)|(\n)/g;
+    DELIMITER_2 = /(print)|(while)|(if)/g;
+    DELIMITER_3 = /(int)/g;
 
     // Match for anything but whitespace
     if (/\S/.test(sourceCode)) {
@@ -46,11 +52,39 @@ function lex() {
         for (var i = 0; i < lines.length; i++) {
             lineNum++;
             line = lines[i].trim();
-            line = line.split(DELIMITER_PATTERN);
-            line = line.filter(checkUndefined);
-            for (var j = 0; j < line.length; j++) {
-                lexeme = line[j];
 
+            line = line.split(DELIMITER_1);
+            line = line.filter(checkUndefined);
+
+            line2 = [];
+
+            for (var j = 0; j < line.length; j++) {
+                newLine = line[j].split(DELIMITER_2);
+                newLine = newLine.filter(checkUndefined);
+                for (var k = 0; k < newLine.length; k++) {
+                    if (!newLine[k].match(/^\s$/) && newLine[k] != "") {
+                            if (!newLine[k].match(/^print$/) && !newLine[k].match(/^(\/\*[^\/\*]*\*\/)$/)) {
+                                newerLine = newLine[k].split(DELIMITER_3);
+                                for (var l = 0; l < newerLine.length; l++) {
+                                    line2.push(newerLine[l]);
+                                }
+                            } else {
+                                line2.push(newLine[k]);
+                            }
+                    }
+                }
+            }
+
+            for (var l = 0; l < line2.length; l++) {
+                document.getElementById("lineOutput").value += "[" + line2[l] + "] ";
+            }
+
+            document.getElementById("lineOutput").value += "\n";
+
+            line = line2;
+
+            for (var l = 0; l < line.length; l++) {
+                lexeme = line[l];
                 // TODO: assess whitespace, comments, and invalid lexemes
                 if (lexeme === "") {
                     // do nothing, ignore whitespace
@@ -61,29 +95,36 @@ function lex() {
                 else if (isValid(lexeme)) {
                     newToken = Token.build(getKind(lexeme), lexeme, lineNum)
                     tokens.push(newToken);
-                    /*alert("LEXER: " + newToken.kind.name);*/
+                    /!*alert("LEXER: " + newToken.kind.name);*!/
                     document.getElementById("taOutput").value += "LEXER --> | " +
                         newToken.kind.name + " [ " + newToken.value + " ] " +
                         " on line " + lineNum + "..." + "\n";
                 } else {
-
-                    // TODO: set up error cases
-
+                    document.getElementById("taOutput").value += "LEXER --> | " +
+                        "ERROR: Invalid lexeme found on line " + lineNum + "\n";
+                    // TODO:
                 }
 
             }
 
         }
 
+        // Check for $[EOP] marker, add $[EOP] marker if not it is not found
         if (tokens[tokens.length - 1].kind != Token.Kind.END_OF_FILE) {
-            // TODO: set up error cases
-            alert("test");
+            newToken = Token.build(Token.Kind.END_OF_FILE, "$", (tokens.length - 1))
+            tokens.push(newToken);
+            document.getElementById("taOutput").value += "LEXER --> | " +
+                "ERROR: Missing $[EOP] marker on last line" + "\n";
+            document.getElementById("taOutput").value += "LEXER --> | " +
+                "$[EOP] marker added to last line" + "\n";
+            document.getElementById("taInput").value += "$\n";
+            return tokens;
         }
 
         return tokens;
 
     } else {
-
+        // No input provided
         // TODO: set up error cases
         return tokens;
 
--- Scripts/tokens.js
+++ Scripts/tokens.js
@@ -104,3 +104,47 @@ Token.Kind = {
         pattern: /^\$$/
     }
 }
+
+/*
+ * Project Grammar Reference
+ *
+ * Program       ::== Block $
+ * Block         ::== { StatementList }
+ * StatementList ::== Statement StatementList
+ * Statement     ::== PrintStatement
+ *               ::== AssignmentStatement
+ *               ::== VarDecl
+ *               ::== WhileStatement
+ *               ::== IfStatement
+ *               ::== Block
+ *
+ * PrintStatement      ::== print( Expr )
+ * AssignmentStatement ::== Id = Expr
+ * VarDecl             ::== type Id
+ * WhileStatement      ::== while BooleanExpr Block
+ * IfStatement         ::== if BooleanExpr Block
+ *
+ * Expr        ::== IntExpr
+ *             ::== StringExpr
+ *             ::== BooleanExpr
+ *             ::== Id
+ * IntExp      ::== digit intop Expr
+ *             ::== digit
+ * StringExpr  ::== " CharList "
+ * BooleanExpr ::== ( Expr boolop Expr)
+ *             ::== boolval
+ *             ::== char
+ * Id          ::== char
+ * CharList    ::== char CharList
+ *             ::== space CharList
+ *             ::== e
+ *
+ * type    ::== int | string | boolean
+ * char    ::== a | b | c | ... | z
+ * space   ::==  the space character
+ * digit   ::==  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+ * boolop  ::==  == | !=
+ * boolval ::== false | true
+ * intop   ::== +
+ *
+ */
